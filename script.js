ea01e8ffdc159316bbd912c0cb4f3cd5dc2c7ad0
arham <arham29april@gmail.com>
2026-02-26 12:14:31 +0000

    cleanup

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ const gameBoard = (() => {
 
     const gameWon = () => {
         if((boardArray[0] === boardArray[1] && boardArray[1] === boardArray[2]) && (boardArray[0] != '' && boardArray[1] != '' && boardArray[2] != '')){
-            return [0,1,2];
+            return true;
         }
         else if((boardArray[3] === boardArray[4] && boardArray[4] === boardArray[5]) && (boardArray[3] != '' && boardArray[4] != '' && boardArray[5] != '')){
-            return [3,4,5];
+            return true;
         }
         else if((boardArray[6] === boardArray[7] && boardArray[7] === boardArray[8]) && (boardArray[6] != '' && boardArray[7] != '' && boardArray[8] != '')){
-            return [6,7,8];
+            return true;
         }
         else if((boardArray[0] === boardArray[3] && boardArray[3] === boardArray[6]) && (boardArray[0] != '' && boardArray[3] != '' && boardArray[6] != '')){
-            return [0,3,6];
+            return true;
         }
         else if((boardArray[1] === boardArray[4] && boardArray[4] === boardArray[7]) && (boardArray[1] != '' && boardArray[4] != '' && boardArray[7] != '')){
-            return [1,4,7];
+            return true;
         }
         else if((boardArray[2] === boardArray[5] && boardArray[5] === boardArray[8]) && (boardArray[2] != '' && boardArray[5] != '' && boardArray[8] != '')){
-            return [2,5,8];
+            return true;
         }
         else if((boardArray[0] === boardArray[4] && boardArray[4] === boardArray[8]) && (boardArray[0] != '' && boardArray[4] != '' && boardArray[8] != '')){
-            return [0,4,8];
+            return true;
         }
         else if((boardArray[2] === boardArray[4] && boardArray[4] === boardArray[6]) && (boardArray[2] != '' && boardArray[4] != '' && boardArray[6] != '')){
-            return [2,4,6];
+            return true;
         }
 
         return false;
